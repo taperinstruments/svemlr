c32cb5f5d519e167674d535565ff143ef7dc21f1
Dom Christie <christiedom@gmail.com>
2021-10-23 21:51:29 -0700

    Scaffold basic channel API

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,21 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Channel from './lib/Channel.svelte'
+  let channels = Array(8).fill(null).map((ch, i) => ({ id: i }))
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <table>
+    <thead>
+    <th></th>
+    <th>Octave</th>
+    <th>Speed</th>
+    <th></th>
+    <th>Group</th>
+    </thead>
+    <tbody>
+      {#each channels as channel}
+        <Channel {...channel} />
+      {/each}
+    </tbody>
+  </table>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
---
+++ src/lib/Channel.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let id
+	export let stepCount = 16
+	export let audio
+	export let group = 0
+	export let octave = 0
+	export let reverse = false
+	export let duration = 1
+	export let active = false
+	export let loopStart = 0
+	export let loopEnd
+
+	let speed = 1
+	let steps = Array(stepCount).fill(false)
+	let activeStep
+</script>
+
+<tr>
+	<td>
+		{#each steps as step, i}
+			<input name="channel_{id}_step" type="radio" value={i} bind:group={activeStep}>
+		{/each}
+	</td>
+	<td>
+		<span>{octave}</span>
+		<button on:click={() => --octave} aria-label="Decrement octave">-</button>
+		<button on:click={() => ++octave} aria-label="Increment octave">+</button>
+	</td>
+	<td>
+		{speed}
+	</td>
+	<td>
+		<input id="channel_{id}_reverse" type="checkbox" bind:checked={reverse}>
+		<label for="channel_{id}_reverse">Rev</label>
+	</td>
+
+	<td>
+		<input name="channel_{id}_group" type="radio" value={0} bind:group={group}>
+		<input name="channel_{id}_group" type="radio" value={1} bind:group={group}>
+		<input name="channel_{id}_group" type="radio" value={2} bind:group={group}>
+		<input name="channel_{id}_group" type="radio" value={3} bind:group={group}>
+	</td>
+</tr>
