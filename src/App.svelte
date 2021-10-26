587c7f44c93e5a61a825271d6ddb138702783622
Dom Christie <christiedom@gmail.com>
2021-10-25 18:36:46 -0700

    Tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,24 @@
 <script>
-  import Channel from './lib/Channel.svelte'
-  let channels = Array(8).fill(null).map((ch, i) => ({ id: i }))
+	import Channel from './lib/Channel.svelte'
+	import Time from './lib/Time.svelte'
+	let channels = Array(8).fill(null).map((ch, i) => ({ id: i }))
 </script>
 
 <main>
-  <table>
-    <thead>
-    <th></th>
-    <th>Octave</th>
-    <th>Speed</th>
-    <th></th>
-    <th>Group</th>
-    </thead>
-    <tbody>
-      {#each channels as channel}
-        <Channel {...channel} />
-      {/each}
-    </tbody>
-  </table>
+	<table>
+		<thead>
+			<th></th>
+			<th>Octave</th>
+			<th>Speed</th>
+			<th></th>
+			<th>Group</th>
+		</thead>
+		<tbody>
+			{#each channels as channel}
+				<Channel {...channel} />
+			{/each}
+		</tbody>
+	</table>
+
+	<Time />
 </main>
