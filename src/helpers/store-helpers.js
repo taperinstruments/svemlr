3189a8acbcc3bf67e771d63a7bf91fa247f86325
Dom Christie <christiedom@gmail.com>
2022-01-28 12:19:54 -0800

    Alternative subscribe with derived store

--- src/helpers/store-helpers.js
+++ src/helpers/store-helpers.js
@@ -1,12 +1,7 @@
+import { derived } from 'svelte/store'
+
 export function subscribe (stores, callback) {
-  const values = []
-  const unsubscribes = stores.map((store, i) => {
-    return store.subscribe(value => {
-      values[i] = value
-      callback(values)
-    })
-  })
-  return function unsubscribe () {
-    unsubscribes.forEach(unsubscribe => unsubscribe())
-  }
+  const store = derived(stores, callback)
+  const unsubscribe = store.subscribe(() => {})
+  return unsubscribe
 }
