d3e29b74cd22b147ca7f55445e0e9d2253dfd775
Dom Christie <christiedom@gmail.com>
2022-02-04 11:30:43 -0800

    Enlarge droppable area

--- src/components/Files.svelte
+++ src/components/Files.svelte
@@ -3,13 +3,43 @@
 </script>
 
 <section>
-  <input accept="audio/*" bind:files={$files} id="files" name="files" type="file" />
+  <label for="files">Audio file drop</label>
+  <div class="file-drop">
+    <input accept="audio/*" bind:files={$files} id="files" name="files" type="file" />
 
-  {#if $files.length}
-    <ul>
-    {#each $files as {name}}
-      <li>{name}</li>
-    {/each}
-    </ul>
-  {/if}
+    {#if $files.length}
+      <ul>
+      {#each $files as {name}}
+        <li>{name}</li>
+      {/each}
+      </ul>
+    {/if}
+  </div>
 </section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+  .file-drop {
+    position: relative;
+    flex-grow: 1;
+    background-color: lightgray;
+  }
+  input[type=file] {
+    opacity: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  ul {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
